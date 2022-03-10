4c749a37109d6ac3e405a70783ec5388e6e33c51
sayhoney1023 <74403474+sayhoney1023@users.noreply.github.com>
2022-03-10 22:22:58 +0900

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,30 @@
+a=10
+b=20
+c=a+b
+
+print(a)
+print(c) 
+print(b)
+
+name="정세헌"
+grade="A+"
+total= 98
+s = 200+300
+
+print(name , grade , total, s) 
+
+print(c*s)
+
+a= "apple"
+print("사과=",a)
+b= "orange"
+c="banana"
+print("바나나=",c)
+
+idrum = input("당신의 이름은 무엇인가요?")
+print("안녕,",idrum)
+
+yyyy = int(input ("당신의 출생년도는 언제인가요?"))
+print("출생년도:", yyyy)
+nai= 2022-(yyyy)
+print("당신의 나이는 만", nai,"세 입니다")
