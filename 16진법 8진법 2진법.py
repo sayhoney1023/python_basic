89b207933de520ef1243b99b16090ff2dc5e5c33
sayhoney1023 <74403474+sayhoney1023@users.noreply.github.com>
2022-03-15 12:58:09 +0900

    Add files via upload
    16진법 8진법 2진법 처음 배운것

---
+++ 16진법 8진법 2진법.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 15 08:53:13 2022
+
+@author: PC
+"""
+
+hello='안녕 파이썬'
+hello2="안녕 파이썬"
+print(hello)
+print(hello2)
+
+fruit1='apple'
+fruit2='banana'
+print(fruit1)
+student_phone="010-4465-6698"
+print(student_phone,fruit2)
+b=0xCF
+print(b)
+c=0o27
+print(c)
+d=0b1011
+print(d)
+print(b+d)
+print(b*c)
+f=0xBC
+print(f)
